ab1d9c630f65979c24d174ed9acd35f16ae442bb
narendra-1106 <narendra1jagtap@gmail.com>
2025-09-26 00:04:01 +0530

    Day 3

--- Oops.java
+++ Oops.java
@@ -1,6 +1,8 @@
 public class Oops {
         public static void main(String[] args) {
-            
+            Pen p1=new Pen();
+            p1.setcolor("Blue");
+            System.err.println(p1.color);
         }
 }
 class Pen {
