b969a3908350a7cbdd9303c3ca5bf97f21b12d1a
narendra-1106 <narendra1jagtap@gmail.com>
2025-09-28 17:08:43 +0530

    Day 3

--- input.java
+++ input.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String[]args) {
-        Scanner sc =new Scanner(System.in);
+        //Scanner sc =new Scanner(System.in);
         /* String input= sc.next();
         System.out.println(input);*/
 
